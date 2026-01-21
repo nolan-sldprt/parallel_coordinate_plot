b00b1c403cfa4e8a409c98a956a3bd6fdc9f08a8
Nolan Cain <nolan.cain@dal.ca>
2026-01-21 18:29:30 -0400

    added fish for a boolean value example

--- examples/demo.py
+++ examples/demo.py
@@ -3,14 +3,15 @@ import matplotlib.pyplot as plt
 import parallel_coordinate_plot
 
 def main():
-    headers: list[str] = ["legs", "arms", "stomachs", "average height (m)", "colour", "tails"]
+    headers: list[str] = ["legs", "arms", "stomachs", "average height (m)", "colour", "tails", "aquatic\nrespiration"]
 
     content: dict[str, list] = {
-        "cow": [4, 0, 4, 1.575, "white", 1],
-        "pig": [4, 0, 1, 0.75, "pink", 1],
-        "dog": [4, 0, 1, 0.7, "white", 1],
-        "snake": [0, 0, 1, 0.6, "green", 1],
-        "gorilla": [2, 2, 1, 1.5, "black", 0]
+        "cow": [4, 0, 4, 1.575, "white", 1, False],
+        "pig": [4, 0, 1, 0.75, "pink", 1, False],
+        "dog": [4, 0, 1, 0.7, "white", 1, False],
+        "snake": [0, 0, 1, 0.6, "green", 1, False],
+        "gorilla": [2, 2, 1, 1.5, "black", 0, False],
+        "fish": [0, 0, 1, 0.3, "grey", 1, True],
     }
 
     parallel_coordinate_plot.plot(
